3819c16f115297e551be3c95bec4467901d24a5f
txagg1992 <curiousaps@gmail.com>
2020-01-27 15:52:07 -0600

    Set up recyclerView. Minor UI changes.

--- app/src/main/java/com/example/recipefinder/activities/RecipeList.kt
+++ app/src/main/java/com/example/recipefinder/activities/RecipeList.kt
@@ -3,6 +3,8 @@ package com.example.recipefinder.activities
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -10,8 +12,10 @@ import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.recipefinder.R
+import com.example.recipefinder.data.RecipeListAdapter
 import com.example.recipefinder.model.Recipe
 import com.example.recipefinder.model.url
+import kotlinx.android.synthetic.main.activity_recipe_list.*
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -20,6 +24,8 @@ class RecipeList : AppCompatActivity() {
 
     private var volleyRequest: RequestQueue? = null
     private var recipeArray: ArrayList<Recipe>? = null
+    private var mRecipeAdapter: RecipeListAdapter? = null
+    private var mLayoutManager: RecyclerView.LayoutManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +40,8 @@ class RecipeList : AppCompatActivity() {
     private fun getRecipe(Url: String){
 
         recipeArray = ArrayList<Recipe>()
+        mRecipeAdapter = RecipeListAdapter(recipeArray!!, this)
+        mLayoutManager = LinearLayoutManager(this)
 
         val recipeRequest =JsonObjectRequest(Request.Method.GET, Url, null,
             Response.Listener {response: JSONObject ->
@@ -53,13 +61,17 @@ class RecipeList : AppCompatActivity() {
 
                         var recipe = Recipe()
                         recipe.mTitle = title
-                        recipe.mIngredients = ingredients
+                        recipe.mIngredients = "Photo Tags: $ingredients"
                         recipe.mThumbnail = thumbnail
                         recipe.mLinkAddress = linkUrl
 
                         //val recipeArray1 = recipeArray
                         recipeArray?.add(recipe)
+                        setupRecyclerView()
                     }
+
+                    mRecipeAdapter!!.notifyDataSetChanged()
+
                 }catch (e: JSONException){
                     e.printStackTrace()
                 }
@@ -73,5 +85,10 @@ class RecipeList : AppCompatActivity() {
             })
 
         volleyRequest?.add(recipeRequest)
+    }
+
+    private fun setupRecyclerView(){
+        recipeRecyclerView.layoutManager = mLayoutManager
+        recipeRecyclerView.adapter = mRecipeAdapter
     }
 }
